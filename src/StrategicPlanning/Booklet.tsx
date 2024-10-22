377cf2810dfb85623b9dc0e2939063f7d340f1e6
Lander Guevarra <landerguevarra.blink@gmail.com>
2024-10-22 10:06:00 +0800

    dummy content placed

--- src/StrategicPlanning/Booklet.tsx
+++ src/StrategicPlanning/Booklet.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import HTMLFlipBook from "react-pageflip";
+import Page1 from "../Assets/BookletContent/Pg1.jpg";
+import Page2 from "../Assets/BookletContent/Pg2.jpg";
+import Page3 from "../Assets/BookletContent/Pg3.jpg";
+import Page4 from "../Assets/BookletContent/Pg4.jpg";
+import Page5 from "../Assets/BookletContent/Pg5.jpg";
+import Page6 from "../Assets/BookletContent/Pg6.jpg";
+import Page7 from "../Assets/BookletContent/Pg7.jpg";
 
 const Book: React.FC = () => {
   return (
@@ -32,25 +39,39 @@ const Book: React.FC = () => {
         disableFlipByClick={false} // Set to true to disable flipping by clicking
       >
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 1</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page1} alt="Page 1" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 2</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page2} alt="Page 2" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 3</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page3} alt="Page 3" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 4</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page4} alt="Page 4" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 5</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page5} alt="Page 5" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 6</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page6} alt="Page 6" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
-          <h1 className="text-3xl font-bold">Page 7</h1>
+          <h1 className="text-3xl font-bold">
+            <img src={Page7} alt="Page 7" />
+          </h1>
         </div>
         <div className="page bg-white flex justify-center items-center">
           <h1 className="text-3xl font-bold">Page 8</h1>
