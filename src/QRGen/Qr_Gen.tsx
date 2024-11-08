9f508c2d0c8f6d75a4135f0564e3b9798f44c5fc
Lander Guevarra <landerguevarra.blink@gmail.com>
2024-11-08 16:07:13 +0800

    done design add confetti

--- src/QRGen/Qr_Gen.tsx
+++ src/QRGen/Qr_Gen.tsx
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QRCode from "qrcode";
-import LeafDivider from "../Assets/leaf.png";
 import QRTicket from "../Assets/PHILRICE_QR_CONFIRMATION.png";
-import html2canvas from "html2canvas"; // Import html2canvas
+import html2canvas from "html2canvas";
 import { get, ref, query, orderByChild, equalTo } from "firebase/database";
 import { database } from "../firebase";
+import Confetti from "react-confetti";
 
 export default function QrGen() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [qrCodeUrl, setQrCodeUrl] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null); // Error state for invalid email
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    // Prevent horizontal scroll on the entire page
+    document.body.style.overflowX = "hidden";
+    document.documentElement.style.overflowX = "hidden";
+
+    // Cleanup to restore overflowX setting on unmount
+    return () => {
+      document.body.style.overflowX = "auto";
+      document.documentElement.style.overflowX = "auto";
+    };
+  }, []);
 
   const generateQRCode = async () => {
     if (email) {
@@ -32,6 +50,8 @@ export default function QrGen() {
           const qrCodeData = await QRCode.toDataURL(email);
           setQrCodeUrl(qrCodeData);
           setError(null); // Clear error if email is valid
+          setShowConfetti(true);
+          setTimeout(() => setShowConfetti(false), 8000);
         } else {
           setError("Invalid email!"); // Email not found in Firebase
           setQrCodeUrl(null); // Clear QR code if email is invalid
@@ -66,86 +86,102 @@ export default function QrGen() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center px-4 w-screen my-24 min-w-[320px] p-4">
-      <div className="text-[#0E9046] text-center text-[40px] md:text-[50px] lg:text-[60px] font-bold">
-        QR Code<span className="text-[#F3B71C]">Generator</span>
-      </div>
-      <img src={LeafDivider} alt="Leaf Divider" className="my-4" />
-
-      {/* Form */}
-      <div className="flex flex-col lg:flex-row items-center gap-4 mt-8 max-w-[768px]">
-        <div className="flex flex-col gap-4">
-          <div className="max-w-[400px]">
-            Lorem ipsum dolor sit amet consectetur. Ultrices nibh rhoncus
-            viverra diam adipiscing. Dui in pharetra sed porttitor justo
-            fermentum sapien mattis consequat.{" "}
-          </div>
-          <input
-            type="text"
-            placeholder="Enter your First Name, Last Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="border border-gray-300 rounded p-2 w-full max-w-md"
-          />
-          <input
-            type="email"
-            placeholder="Enter your Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border border-gray-300 rounded p-2 w-full max-w-md"
-          />
-          <button
-            onClick={generateQRCode} // Generate QR code on button click
-            className="bg-[#F3BD1C] text-white font-bold py-3 px-6 lg:px-12 rounded-md"
-          >
-            Generate
-          </button>
+    <div className="flex flex-col lg:flex-row justify-center items-center px-4 w-screen h-full my-20 p-4 gap-4 overflow-hidden">
+      <div className="w-full lg:w-[734px] h-auto lg:h-[744px] border-[1px] border-solid border-gray-100 rounded-2xl px-16 py-16 flex flex-col justify-center items-center shadow-lg drop-shadow-md backdrop-blur-lg">
+        <div className="text-[#0E9046] text-center text-[40px] md:text-[50px] lg:text-[60px] font-bold">
+          Get Event-Ready with <br />
+          Your<span className="text-[#F3B71C]"> QR Code</span>
         </div>
+        {/* <img src={LeafDivider} alt="Leaf Divider" className="my-4" /> */}
+        <div className="text-[22px] text-center my-4">
+          Confirm registration and save your QR Code for <br />
+          easy check-in at the event!
+        </div>
+        <div className="w-8/12 h-[1px] bg-[#0E9046] my-4" />
 
-        {/* Display error or QR Code */}
-        {error && (
+        {/* Form */}
+        <div className="flex flex-col lg:flex-row items-center gap-4 mt-8">
+          <div className="flex flex-col gap-4 w-[250px] md:w-[450px]">
+            <input
+              type="text"
+              placeholder="Enter your First Name, Last Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border border-gray-300 rounded p-2"
+            />
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 rounded p-2"
+            />
+            <button
+              onClick={generateQRCode} // Generate QR code on button click
+              className="bg-[#F3BD1C] text-white font-bold py-3 px-6 lg:px-12 rounded-md"
+            >
+              Generate
+            </button>
+          </div>
+        </div>
+      </div>
+      {qrCodeUrl && (
+        <>
+          {showConfetti && (
+            <Confetti
+              width={window.innerWidth} // Fixed width to prevent horizontal scroll
+              height={document.documentElement.scrollHeight}
+              recycle={false}
+              numberOfPieces={2000}
+              colors={["#0C6972", "#EFB71E", "#EFB71E", "#FFFFFF"]}
+            />
+          )}
+          <div className="w-full lg:w-[734px] h-auto lg:h-[744px] border-[1px] border-solid border-gray-100 rounded-2xl px-16 py-16 flex flex-col justify-center items-center shadow-lg drop-shadow-md backdrop-blur-lg">
+            <div className="flex flex-col justify-center items-center">
+              <div
+                id="qr-section"
+                className="flex flex-col items-center mb-8 min-w-[320px] max-w-[375px] h-[608px]"
+              >
+                <div className="flex flex-col items-center mt-8 font-bold">
+                  <div className="absolute z-30 mt-[100px] text-[#0C6972] text-2xl mr-2">
+                    YOU'RE ALL SET!
+                  </div>
+                  <div className="absolute z-30 mt-[130px] text-sm flex flex-col items-center justify-center text-center mr-2">
+                    See you, {name} <br />
+                    Your registration is complete!
+                  </div>
+                  <img
+                    src={QRTicket}
+                    alt="QR Ticket"
+                    className="absolute z-10 w-80"
+                  />
+                  <img
+                    src={qrCodeUrl}
+                    alt="QR Code"
+                    className="w-44 h-44 z-20 relative mt-[190px] mr-[10px]"
+                  />
+                </div>
+              </div>
+
+              {/* DOWNLOAD Button */}
+              <button
+                onClick={handleDownload} // Trigger the download function on button click
+                className="bg-[#F3BD1C] text-white font-bold py-3 px-6 lg:px-12 rounded-md"
+              >
+                DOWNLOAD
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+      {/* Display error or QR Code */}
+      {error && (
+        <div className="w-full lg:w-[734px] border-[1px] border-solid border-gray-100 rounded-2xl px-16 py-16 flex flex-col justify-center items-center shadow-lg drop-shadow-md backdrop-blur-lg">
           <div className="text-red-500 text-xl font-bold mt-4">
             {error} {/* Display error message */}
           </div>
-        )}
-
-        {qrCodeUrl && (
-          <div className="flex flex-col justify-center items-center">
-            <div
-              id="qr-section"
-              className="flex flex-col items-center mb-8 min-w-[320px] max-w-[375px] h-[608px]"
-            >
-              <div className="flex flex-col items-center mt-8">
-                <div className="absolute z-30 mt-[100px] font-bold text-[#0C6972] text-2xl">
-                  YOU'RE ALL SET!
-                </div>
-                <div className="absolute z-30 mt-[130px] font-bold text-sm flex flex-col items-center justify-center text-center">
-                  See you, {name} <br />
-                  Your registration is complete!
-                </div>
-                <img
-                  src={QRTicket}
-                  alt="QR Ticket"
-                  className="absolute z-10 w-80"
-                />
-                <img
-                  src={qrCodeUrl}
-                  alt="QR Code"
-                  className="w-44 h-44 z-20 relative mt-[190px] mr-[10px]"
-                />
-              </div>
-            </div>
-
-            {/* DOWNLOAD Button */}
-            <button
-              onClick={handleDownload} // Trigger the download function on button click
-              className="bg-[#F3BD1C] text-white font-bold py-3 px-6 lg:px-12 rounded-md"
-            >
-              DOWNLOAD
-            </button>
-          </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
