3c1e515bbcbe7ed1ed72d67a160e3ec34faa4b3b
Blink <gjlabastilla@gmail.com>
2024-11-25 16:52:57 +0800

    changes01

--- src/Conference/Programs/ProgramSum.tsx
+++ src/Conference/Programs/ProgramSum.tsx
@@ -6,7 +6,7 @@ export default function ProgramSum() {
     <div className="flex flex-col justify-center items-center">
       <img
         src={Sched}
-        className="min-w-[500px] md:w-[80%] h-auto rotate-90 md:rotate-0 my-[160px] md:my-[100px]"
+        className="min-w-[400px] md:w-[80%] h-auto rotate-90 md:rotate-0 my-[160px] md:my-[100px]"
       />
     </div>
   );
